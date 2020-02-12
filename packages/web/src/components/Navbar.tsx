f86ad19f19d4bd28cd3d042e2fa0461993be75a0
Royal <tomburridge2002@gmail.com>
2020-02-12 20:10:19 +0000

    [WEB] Progess on Home PAge
    Add Menu

--- packages/web/src/components/Navbar.tsx
+++ packages/web/src/components/Navbar.tsx
@@ -1,35 +1,62 @@
 import React from 'react';
 import Styled from 'styled-components';
-import { ReactComponent as CornerHome } from '../Assets/HomeCorner.svg';
+import BackgroundImg from '../Assets/Group 5.png';
+import Logo from '../Assets/MyFolio.png';
+import { ReactComponent as MenuSvg } from '../Assets/Icons/HamburgerMenu.svg';
 
 const PageContainer = Styled.div`
-    max-width: 100vw;
-    max-height: 100vh;
+    width: 100vw;
+    height: 100vh;
+
+    background-image: url('${BackgroundImg}');
+    background-size: calc(100vw + 180px) calc(100vh + 130px);
+    background-position: -51px -43px;
+    background-repeat: no-repeat;
 
     overflow: hidden;
 `;
 
-const Corner1 = Styled(CornerHome)`
-    position: relative;
-    
-    top: -60px;
-    left: calc(100vw - 1077px);
+const CenterLogo = Styled.img`
+    width: 30vw;
+    position:absolute;
+    top:0; left:0; right:0; bottom:0;
+    margin:auto;
+
 `;
 
-const Corner2 = Styled(CornerHome)`
-    position: relative;
-    
-    top: calc(100vh - 1777px);
-    left: -60px;
+const MenuIcon = Styled(MenuSvg)`
+    width: 40px;
+    height: 40px;
+`;
 
-    transform: rotate(180deg);
+const MenuButton = Styled.button`
+    outline: none;
+    width: 50px;
+    height: 50px;
+    position: absolute;
+    top: 15px;
+    right: 20px;
+    background: none;
+    border: none;
+
+    transition: 0.2s ease-in-out;
+
+    &:hover {
+        transform: scale(1.2);
+    }
+
+    &:active {
+        transform: scale(0.9);
+    }
 `;
 
 const Navbar: React.FunctionComponent = () => {
     return (
         <PageContainer>
-            <Corner1></Corner1>
-            <Corner2></Corner2>
+            <MenuButton>
+                <MenuIcon />
+            </MenuButton>
+            <CenterLogo src={Logo}></CenterLogo>
         </PageContainer>
     );
 };
